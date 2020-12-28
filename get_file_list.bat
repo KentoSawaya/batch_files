2cf2fce961faa6a3d54927e82f937509da89e611
KentoSawaya <kento.sawaya.0112@gmail.com>
2020-12-28 17:02:27 +0900

    directory を出力しないよう get_file_list.bat を修正

--- get_file_list.bat
+++ get_file_list.bat
@@ -1,4 +1,4 @@
-dir /b > file_list_1.txt
+dir /A:-D /b > file_list_1.txt
 findstr /X /V file_list_1.txt file_list_1.txt > file_list_2.txt
 findstr /X /V get_file_list.bat file_list_2.txt > file_list_3.txt
 findstr /X /V file_list.txt file_list_3.txt > file_list.txt
